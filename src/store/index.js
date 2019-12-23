3bf5b9f81fe6cd2cc43bb40ea39c7b8717bec7e2
zoe-zachary <zoezachary27@gmail.com>
2019-12-23 15:38:08 +0700

    init and some starter code

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,67 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
+var axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posts: [],
+    comments: []
   },
   mutations: {
+    setPosts(state, posts) {
+      console.log('VUEX MUTATION: setPosts')
+
+      state.posts = posts
+    },
+
+    addPost(state, post) {
+      console.log('VUEX MUTATION: addPost\nIncoming post data:', post)
+
+      state.posts.push(post)
+      console.log('VUEX MUTATION: addPost completed\nShowing current data:', state.posts)
+    },
+
+    setComments(state, comments) {
+      console.log('VUEX MUTATION: setComments')
+
+      state.comments = comments
+    }
   },
   actions: {
+    async fetchPosts({commit}) {
+      try {
+        console.log('VUEX ACTION: fetchPosts')
+
+        const {data} = await axiosInstance.get('/posts')
+
+        commit('setPosts', data)
+        
+        return data
+      }
+      catch(err) {
+        console.log(err.response.data.message)
+      }
+    },
+
+    async fetchComments({commit}) {
+      try {
+        console.log('VUEX ACTION: fetchComments')
+
+        const {data} = await axiosInstance.get('/comments')
+        
+        commit('setComments', data)
+      }
+      catch(err) {
+        console.log(err.response.data.message)
+      }
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>this is from mapState posts</p>
+    <div v-for="post in posts" :key="post.id">
+      <p>{{post.title}}</p>
+      <p>{{post.author}}</p>
+    </div>
+    
+    <hr>
+
+    <p> this is from data dataPosts</p>
+    <div v-for="post in dataPosts" :key="post.id + 'a'">
+      <p>{{post.title}}</p>
+      <p>{{post.author}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mapState, mapActions, mapMutations} from 'vuex'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      dataPosts: [],
+      dataComments: []
+    }
+  },
+  
+  methods: {
+    ...mapActions(['fetchPosts', 'fetchComments']),
+    ...mapMutations(['addPost'])
+  },
+
+  computed: {
+    ...mapState(['posts', 'comments'])
+  },
+
+  beforeCreate() {
+    console.log('VM LIFECYCLE: beforeCreate')
+    // this.fetchPosts()
+    // this.fetchPosts() will get an error message: Error in beforeCreate hook: "TypeError: this.fetchPosts is not a function"
+  },
+
+  created() {
+    console.log('VM LIFECYLCLE: created')
+
+    // Using "then" will virtually making fetchPosts to fetchComments transition synchronous
+    // console.log output:
+    // fetchPosts -> setPosts -> fetchComments -> setComments
+    this.fetchPosts().then(posts => {
+      this.fetchComments()
+
+      // the DOM that uses dataPosts will normally show the same data as posts from the store
+      this.dataPosts = posts
+    })
+
+    // Without "then", however, the fetching will be invoked asynchronously
+    // console.log output:
+    // fetchPosts -> fetchComments -> setPosts -> setComments
+    // this.fetchPosts()then
+    // this.fetchComments()
+  },
+
+  mounted() {
+    console.log('Manually adding data to posts in the store')
+    this.addPost({
+      id: 2,
+      title: 'Second post for the store posts',
+      author: 'zoezachary'
+    })
+
+    console.log('Manually adding data to data dataPosts')
+    this.dataPosts.push({
+      id: 2,
+      title: 'Second post for data dataPosts',
+      author: 'zoezachary'
+    })
+
+    console.log('current posts:', this.posts)
+    console.log('current dataPosts:', this.dataPosts)
   }
 }
 </script>
